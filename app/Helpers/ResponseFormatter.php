22b51d3434b8aedbd3e11f9d7559b1e4db63e38a
Nur Afdlol Musyafa <dddlllooolll@gmail.com>
2022-06-24 01:49:32 +0700

    helpers ResponseFormatter

---
+++ app/Helpers/ResponseFormatter.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Helpers;
+
+/**
+ * Format response.
+ */
+class ResponseFormatter
+{
+    /**
+     * API Response
+     *
+     * @var array
+     */
+    protected static $response = [
+        'meta' => [
+            'code' => 200,
+            'status' => 'success',
+            'message' => null,
+        ],
+        'data' => null,
+    ];
+
+    /**
+     * Kasih response success.
+     */
+    public static function success($data = null, $message = null)
+    {
+        self::$response['meta']['message'] = $message;
+        self::$response['data'] = $data;
+
+        return response()->json(self::$response, self::$response['meta']['code']);
+    }
+
+    /**
+     * Kasih error response.
+     */
+    public static function error($data = null, $message = null, $code = 400)
+    {
+        self::$response['meta']['status'] = 'error';
+        self::$response['meta']['code'] = $code;
+        self::$response['meta']['message'] = $message;
+        self::$response['data'] = $data;
+
+        return response()->json(self::$response, self::$response['meta']['code']);
+    }
+}
