aa8dffbc6e8f1aca2dc60d52b2b203af810816ba
Nur Afdlol Musyafa <dddlllooolll@gmail.com>
2022-06-24 01:18:29 +0700

    migration user,complaint

---
+++ database/migrations/2022_06_23_181213_create_complaints_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComplaintsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('complaints', function (Blueprint $table) {
+            $table->id();
+
+            $table->integer('user_id')->nullable();
+            $table->string('title')->nullable();
+            $table->text('description')->nullable();
+            $table->text('picturePath')->nullable();
+            $table->text('location')->nullable();
+            $table->string('is_public')->nullable();
+            $table->string('is_secret')->nullable();
+            
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('complaints');
+    }
+}
